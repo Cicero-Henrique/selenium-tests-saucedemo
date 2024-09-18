5c0545441e54c9709beded960d4453273713377a
Cicero Henrique <cicero.penha@letscode.com.br>
2024-09-18 17:39:01 -0300

    Add TC-01

--- conftest.py
+++ conftest.py
@@ -1,11 +1,11 @@
 import pytest
 from selenium import webdriver
 
-
+@pytest.fixture
 def setup_teardown():
     global driver
     driver = webdriver.Chrome()
-    driver.implicit_wait(5)
+    driver.implicitly_wait(5)
     driver.maximize_window()
     driver.get("https://www.saucedemo.com/")
 
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,3 @@
-from time import sleep
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 import conftest
 from pages.base_page import BasePage
@@ -7,17 +5,13 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
 
     def __init__(self):
-        driver = conftest.driver
+        self.driver = conftest.driver
         # elements
         self.username_field = (By.ID, "user-name")
         self.password_field = (By.ID, "password")
         self.login_button = (By.ID, "login-button")
 
     def make_login(self, username, password):
-        self.find_element(self.username_field, username)
-        self.find_element(self.password_field, password)
+        self.write(self.username_field, username)
+        self.write(self.password_field, password)
         self.click(self.login_button)
-
-# sleep(3)
-# assert browser.current_url == "https://www.saucedemo.com/inventory.html"
-
