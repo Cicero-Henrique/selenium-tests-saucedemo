c3520323246cf59d5e69b6eff44b0b0f320c4052
Cicero Henrique <cicero.penha@letscode.com.br>
2024-09-19 18:33:25 -0300

    Add test two

--- tests/test_login_success.py
+++ tests/test_login_success.py
@@ -6,8 +6,17 @@ from pages.login_page import LoginPage
 @pytest.mark.usefixtures("setup_teardown")
 class Test01:
 
-    def test_login_success(self):
+    def test_successful_login(self):
         driver = conftest.driver
         login_page = LoginPage()
         login_page.make_login("standard_user", "secret_sauce")
         assert driver.current_url == "https://www.saucedemo.com/inventory.html"
+
+@pytest.mark.usefixtures("setup_teardown")
+class Test02:
+
+    def test_invalid_user(self):
+        driver = conftest.driver
+        login_page = LoginPage()
+        login_page.make_login("wrong_user", "wrong_password")
+        login_page.check_error_message()
