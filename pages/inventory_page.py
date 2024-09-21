22558fad7b45150e683896f503d668e7cb3e4c51
Cicero Henrique <cicero_penha@hotmail.com>
2024-09-21 18:23:09 -0300

    Add TC07

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -11,11 +11,13 @@ class InventoryPage(BasePage):
         text_title = "Swag Labs"
         text_all_items = "All Items"
         text_about = "About"
+        text_logout = "Logout"
         # elements
         self.title = (By.XPATH, f"//div[@class='app_logo' and (text() = '{text_title}')]")
         self.side_menu_button = (By.CLASS_NAME, "bm-burger-button")
-        self.all_items_button = (By.XPATH, f"//a[@href='#' and (text() = '{text_all_items}')]")
+        self.all_items_button = (By.XPATH, f"//a[(text() = '{text_all_items}')]")
         self.about_button = (By.XPATH, f"//a[(text() = '{text_about}')]")
+        self.logout_button = (By.XPATH, f"//a[(text() = '{text_logout}')]")
 
     def check_login_successful(self):
         login_page = LoginPage()
@@ -26,8 +28,20 @@ class InventoryPage(BasePage):
     def open_side_menu(self):
         self.click(self.side_menu_button)
 
-    def click_on_all_items(self):
-        self.click(self.all_items_button)
+    def check_all_items(self):
+        self.is_element_visible(self.all_items_button)
+        href_value = self.find_element(self.all_items_button).get_attribute('href')
+        expected_href = "https://www.saucedemo.com/inventory.html#"
+        assert href_value == expected_href, f"Expected href to be {expected_href}, but got {href_value}"
 
     def check_about_button(self):
         self.is_element_visible(self.about_button)
+        href_value = self.find_element(self.about_button).get_attribute('href')
+        expected_href = "https://saucelabs.com/"
+        assert href_value == expected_href, f"Expected href to be {expected_href}, but got {href_value}"
+
+    def check_logout_user(self):
+        self.click(self.logout_button)
+        href_value = self.find_element(self.logout_button).get_attribute('href')
+        expected_href = "https://www.saucedemo.com/"
+        assert href_value == expected_href, f"Expected href to be {expected_href}, but got {href_value}"
--- tests/test_inventory_page.py
+++ tests/test_inventory_page.py
@@ -1,6 +1,5 @@
 
 import pytest
-import conftest
 from pages.inventory_page import InventoryPage
 
 
@@ -8,21 +7,25 @@ from pages.inventory_page import InventoryPage
 class Test05:
 
     def test_all_items_button(self):
-        driver = conftest.driver
         inventory_page = InventoryPage()
         inventory_page.check_login_successful()
         inventory_page.open_side_menu()
-        inventory_page.click_on_all_items()
-        assert driver.current_url == "https://www.saucedemo.com/inventory.html"
+        inventory_page.check_all_items()
 
 @pytest.mark.usefixtures("setup_teardown")
-class Test05:
+class Test06:
 
     def test_about_button(self):
         inventory_page = InventoryPage()
         inventory_page.check_login_successful()
         inventory_page.open_side_menu()
         inventory_page.check_about_button()
-        href_value = inventory_page.find_element(inventory_page.about_button).get_attribute('href')
-        expected_href = "https://saucelabs.com/"
-        assert href_value == expected_href, f"Expected href to be {expected_href}, but got {href_value}"
+
+@pytest.mark.usefixtures("setup_teardown")
+class Test07:
+
+    def test_logout_button(self):
+        inventory_page = InventoryPage()
+        inventory_page.check_login_successful()
+        inventory_page.open_side_menu()
+        inventory_page.check_logout_user
