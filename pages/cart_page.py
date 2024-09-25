0d5614914e06e2570d05f4b8bb604c4369d8dcd7
Cicero Henrique <cicero_penha@hotmail.com>
2024-09-25 20:51:39 -0300

    Add TC20

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -10,6 +10,11 @@ class CartPage(BasePage):
         self.driver = conftest.driver
         # elements
         self.remove_item = (By.ID, "remove-sauce-labs-backpack")
+        self.checkout_button = (By.ID, "checkout")
+        self.continue_button = (By.ID, "continue")
+        self.first_name_input = (By.ID, "first-name")
+        self.last_name_input = (By.ID, "last-name")
+        self.postal_code_input = (By.ID, "postal-code")
 
     def check_remove_item(self):
         inventory_page = InventoryPage()
@@ -20,3 +25,21 @@ class CartPage(BasePage):
         self.driver.get("https://www.saucedemo.com/cart.html")
         self.click(self.remove_item)
         inventory_page.check_cart_counter("1")
+
+    def complete_form(self):
+        self.write(self.first_name_input, "Elon")
+        self.write(self.last_name_input, "Musk")
+        self.write(self.postal_code_input, "1234")
+        self.click(self.continue_button)
+
+    def check_make_checkout(self):
+        inventory_page = InventoryPage()
+        inventory_page.check_login_successful()
+        inventory_page.add_item_to_cart("sauce-labs-backpack", "1")
+        self.driver.get("https://www.saucedemo.com/cart.html")
+        self.click(self.checkout_button)
+        self.wait_url("https://www.saucedemo.com/checkout-step-one.html")
+        self.complete_form()
+        current_url = self.driver.current_url
+        expected_url = "https://www.saucedemo.com/checkout-step-two.html"
+        assert current_url == expected_url, f"Expected url to be {expected_url}, but got {current_url}"
--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -8,3 +8,10 @@ class Test19:
     def test_product_name(self):
         cart_page = CartPage()
         cart_page.check_remove_item()
+
+@pytest.mark.usefixtures("setup_teardown")
+class Test20:
+
+    def test_product_name(self):
+        cart_page = CartPage()
+        cart_page.check_make_checkout()
