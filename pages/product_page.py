ca21b70e4f0ed235360fc121c1c798fb047cb292
Cicero Henrique <cicero.penha@letscode.com.br>
2024-09-25 17:45:31 -0300

    Use url as variable

--- pages/product_page.py
+++ pages/product_page.py
@@ -17,13 +17,14 @@ class ProductPage(BasePage):
         self.product_price = (By.XPATH, "//div[@data-test='inventory-item-price']")
         self.back_inventory_button = (By.ID, "back-to-products")
         self.add_to_cart_button = (By.ID, "add-to-cart")
+        self.product_url = "https://www.saucedemo.com/inventory-item.html?id=4"
 
     def make_product_assertions(self, inventory, product):
         inventory_page = InventoryPage()
         inventory_page.check_login_successful()
         info_inventory = self.find_element(inventory).text
         self.click(self.inventory_name)
-        self.wait_url("https://www.saucedemo.com/inventory-item.html?id=4")
+        self.wait_url(self.product_url)
         info_product = self.find_element(product).text
         assert info_inventory == info_product, f"Expected product name to be {info_inventory}, but got {info_product}"
 
@@ -41,7 +42,7 @@ class ProductPage(BasePage):
         inventory_page.check_login_successful()
         inventory_url = "https://www.saucedemo.com/inventory.html"
         self.click(self.inventory_name)
-        self.wait_url("https://www.saucedemo.com/inventory-item.html?id=4")
+        self.wait_url(self.product_url)
         self.click(self.back_inventory_button)
         self.wait_url(inventory_url)
         assert inventory_url == self.driver.current_url, f"Expected product name to be {inventory_url}, but got {self.driver.current_url}"
@@ -50,6 +51,6 @@ class ProductPage(BasePage):
         inventory_page = InventoryPage()
         inventory_page.check_login_successful()
         self.click(self.inventory_name)
-        self.wait_url("https://www.saucedemo.com/inventory-item.html?id=4")
+        self.wait_url(self.product_url)
         self.click(self.add_to_cart_button)
         inventory_page.check_cart_counter("1")
